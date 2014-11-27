9b779010cfe7662040f201c25fc2ab12ac6fabe5
Fig Mistwood <fig.mistwood+bb@gmail.com>
2014-11-27 00:18:50 -0600

    QAV-7 Added missing dependencies to Windows zipped deployment package

--- deployment/windows/deploy.bat
+++ deployment/windows/deploy.bat
@@ -1,13 +1,18 @@
 @echo off
 
-set DEPLOYMENT_DIR="%CD%"
+set DEPLOYMENT_DIR=%CD%
 set RESOURCE_DIR=%DEPLOYMENT_DIR%\resources
 
 cd ..\..
 
-set PROJECT_ROOT_DIR="%CD%"
+set PROJECT_ROOT_DIR=%CD%
 
-rmdir /S /Q _build _install
+if exist _build (
+  rmdir /S /Q _build
+)
+if exist _install (
+  rmdir /S /Q _install
+)
 
 call configure
 
@@ -33,16 +38,30 @@ del qt_dir.tmp
 
 cd "%DEPLOYMENT_DIR%"
 
-if exist "%APPLICATION_NAME%" (
-  del /S /Q "%APPLICATION_NAME%"
+set ZIP_DIR_NAME=%APPLICATION_NAME%
+
+if exist "%ZIP_DIR_NAME%" (
+  rmdir /S /Q "%ZIP_DIR_NAME%"
 )
-mkdir "%APPLICATION_NAME%"
-xcopy /E /Y /B "%PROJECT_ROOT_DIR%\_install\*" "%APPLICATION_NAME%"
+mkdir "%ZIP_DIR_NAME%"
+xcopy /E /Y /B "%PROJECT_ROOT_DIR%\_install\*" "%ZIP_DIR_NAME%"
+
+copy "%QT_DIR%\bin\icudt52.dll" "%ZIP_DIR_NAME%"
+copy "%QT_DIR%\bin\icuin52.dll" "%ZIP_DIR_NAME%"
+copy "%QT_DIR%\bin\icuuc52.dll" "%ZIP_DIR_NAME%"
+copy "%QT_DIR%\bin\libgcc_s_dw2-1.dll" "%ZIP_DIR_NAME%"
+copy "%QT_DIR%\bin\libstdc++-6.dll" "%ZIP_DIR_NAME%"
+copy "%QT_DIR%\bin\libwinpthread-1.dll" "%ZIP_DIR_NAME%"
+copy "%QT_DIR%\bin\Qt5Core.dll" "%ZIP_DIR_NAME%"
+copy "%QT_DIR%\bin\Qt5Gui.dll" "%ZIP_DIR_NAME%"
+copy "%QT_DIR%\bin\Qt5Network.dll" "%ZIP_DIR_NAME%"
+copy "%QT_DIR%\bin\Qt5OpenGL.dll" "%ZIP_DIR_NAME%"
+copy "%QT_DIR%\bin\Qt5Widgets.dll" "%ZIP_DIR_NAME%"
 
 set ZIP_FILE_NAME=%APPLICATION_NAME%_%VERSION_NUMBER%.%BUILD_NUMBER%_Windows.zip
-7z a -r "%ZIP_FILE_NAME%" "%APPLICATION_NAME%"
+7z a -r "%ZIP_FILE_NAME%" "%ZIP_DIR_NAME%"
 
-rmdir /S /Q "%APPLICATION_NAME%"
+rmdir /S /Q "%ZIP_DIR_NAME%"
 
 makensis /V3 /NOCD ^
   /DQT_DIR="%QT_DIR%" ^
